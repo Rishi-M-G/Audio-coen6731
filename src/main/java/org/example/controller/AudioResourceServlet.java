1c02fabd3acdd890edc48d3630484ec78f07e184
Rishi M G <rishikrishnan87@gmail.com>
2023-02-26 23:27:27 -0500

    Final Commit

--- src/main/java/org/example/controller/AudioResourceServlet.java
+++ src/main/java/org/example/controller/AudioResourceServlet.java
@@ -53,29 +53,48 @@ public class AudioResourceServlet extends HttpServlet{
 		String track_title = request.getParameter("track_title");
 		Audio song1 = audioDB.get(track_title);
 		String artist_name = song1.getArtistName();
+		String album_title = song1.getAlbumTitle();
+		String track_number = song1.getTrackNumber();
+		int year = song1.getYear();
+		int no_of_reviews = song1.getNoOfReviews();
+		int no_of_copies = song1.getNoOfCopiesSold();
 	
 		
 		
 		Gson gson = new Gson();
 		JsonElement element = gson.toJsonTree(audioDB);
 		
+		
 		PrintWriter out = response.getWriter();
 		response.setContentType("application/json");
 		response.setCharacterEncoding("UTF-8");
 		
-		out.println("GET RESPONSE IN JSON - all elements " + element.toString());
-		out.println("RESPONSE IN JSON - Single Element:"+gson.toJson(artist_name));
-		out.flush();
+		if (song1 == null)
+		{
+			out.println("GET RESPONSE IN JSON - all elements " + element.toString());
+		}
+		else
+		{
+			out.println("RESPONSE IN JSON - Single Element:");
+			out.println("Artist Name:"+gson.toJson(artist_name));
+			out.println("Track Title:"+gson.toJson(track_title));
+			out.println("Album Title:"+gson.toJson(album_title));
+			out.println("Track Number:"+gson.toJson(track_number));
+			out.println("Year:"+gson.toJson(year));
+			out.println("No of Reviews:"+gson.toJson(no_of_reviews));
+			out.println("No of Reviews:"+gson.toJson(no_of_copies));
+			out.flush();
+		}
 		
-		
-		//Response in JSON type
-		
-		out.println("RESPONSE IN JSON - All Elements:" + element.toString());
-		out.flush();
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String track_title = request.getParameter("track_title");
+		String artist_name = request.getParameter("artist_name");
+		String album_title = request.getParameter("album_title");
+		String track_number = request.getParameter("track_number");
 		
+		response.getOutputStream().println("POST RESPONSE: Audio ");
 	}
 }
